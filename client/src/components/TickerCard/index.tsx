032a47d33c8160c55958b71be8675b725f0826d6
Niall-Adappt <niall.m@adappt.ai>
2024-04-02 20:39:36 +0100

    fetch data from component

--- client/src/components/TickerCard/index.tsx
+++ client/src/components/TickerCard/index.tsx
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getPercentageChange } from '@/lib/utils';
 import './style.css';
-import { TickerData } from '@/api';
+import api, { TickerData } from '@/api';
 import { Link } from 'react-router-dom';
 
 interface TickerCardProps {
-  // tickerDetails: TickerDetailsProps | null;
-  // changeSinceLastDay: ChangeSinceLastDayProps | null;
-tickerData: TickerData
+  ticker: string
+  setIsLoading?: (value: boolean | ((prevValue: boolean) => boolean)) => void
+  setError?: (value: boolean | ((prevValue: boolean) => boolean)) => void
 }
 
 const TickerCard: React.FC<TickerCardProps> = ({
-tickerData
+ticker, setIsLoading, setError
 }) => {
+  const [tickerData, setTickerData] = useState<any>(null);
+  const [isMounted, setIsMounted] = useState(false);
+  
+  useEffect(() => {
+    const fetchData = async () => { 
+      try {
+        setIsLoading?.(true);
+
+        const tickerInformation = await api.getTickerData(ticker);
+        // const tickerInformation = resultArray[0] || null;
+        setTickerData(tickerInformation);
+      } catch (error) {
+        setError?.(true);
+        console.error(error);
+      } finally {
+        setIsLoading?.(false);
+      }
+    };
+
+    fetchData();
+  }, [ticker]);
+
+    useEffect(() => {
+      setIsMounted(true);
+    }, []);
+  
+    if (!isMounted || !tickerData) {
+      return null;
+    }
   // if (!name && !logo && !sector) return null;
-  if(!tickerData) return null
-  const  {ticker, tickerInfo, prevOpen, prevClose } = tickerData
-  const {name, logo, sector } = tickerInfo as any
+  const  { price, changePercent, prevOpen, prevClose } = tickerData
 
   const getPriceColor = () => {
     if (!prevOpen || !prevClose) return;
-
     const color = isNegative ? 'red' : '#37AD94';
-
     return { color };
   };
 
-  const isNegative = prevOpen && prevClose
-    ? getPercentageChange(prevOpen, prevClose).isNegative
-    : null;
+  const logo = `https://s3.polygon.io/logos/${ticker.toLowerCase()}/logo.png`
+  const isNegative = Number(changePercent) < 0 ? true : false
+  const sign = isNegative ? '-' : '+';
+  const absolutDiff = (prevClose - price).toFixed(2)
 
   return (
     <div className='ticker-details-box'>
@@ -36,21 +62,21 @@ tickerData
       <img
         className='ticker-logo mx-auto'
         src={logo}
-        alt={`${name} (${ticker})`}
+        alt={`stock logo(${ticker})`}
         width="100"
       />
-      <div className='ticker-name'>{name}</div>
-      <div className='ticker-sector'>{sector}</div>
+      <div className='ticker-name'>{ticker}</div>
+      {/* <div className='ticker-sector'>{sector}</div> */}
       {prevOpen && prevClose && (
         <div className='price-and-color'>
-          <div className='ticker-price'>{prevClose} USD</div>
+          <div className='ticker-price'>{price} USD</div>
+
           <div
             className={`ticker-color ticker-${isNegative ? 'down' : 'up'}`}
             style={getPriceColor()}
           >
             {
-              getPercentageChange(prevOpen, prevClose)
-                .percentage
+              `${sign}${absolutDiff} (${changePercent}%)`
             }
           </div>
         </div>
