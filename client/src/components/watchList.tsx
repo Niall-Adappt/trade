d288e14fcc86cc408613306bd80a675464977482
Niall-Adappt <niall.m@adappt.ai>
2024-04-03 15:44:28 +0100

    fetch watchlist

--- client/src/components/watchList.tsx
+++ client/src/components/watchList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api, { TickerData } from '@/api';
+import api, { TickerData, WatchlistResponse } from '@/api';
 import TickerCard from './TickerCard';
 // import { ChangeSinceLastDayProps } from '@/pages/homePage';
 // import TickerDetails from './TickerDetails';
@@ -8,36 +8,37 @@ import TickerCard from './TickerCard';
   // const watchList = ["TSLA", "AAPL", "GOOGL", "AMZN", "MSFT"];
   
 export const WatchList: React.FC = () => {
-  // const [watchList, setWacthlist] = useState<(string | null)[]>([]);
+  const [watchList, setWatchlist] = useState<string[]>();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
   //replace with fetch watchlist and use the isloading and error there
-  const watchList = ["TSLA"];
+  // const watchList = ["TSLA"];
   
-  // const fetchWatchlist = async () => {
-  //   setIsLoading(true);
-  //   try {
-  //     const data = await api.getWatchlist();
-  //     setWacthlist(data as string[]);
-  //   } catch (err) {
-  //     console.error(err);
-  //     setError(true);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
+  const fetchWatchlist = async () => {
+    setIsLoading(true);
+    try {
+      const data = await api.getWatchlist();
+      console.log(data)
+      setWatchlist(data.watchlist);
+    } catch (err) {
+      console.error(err);
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchWatchlist();
-  // }); 
+  useEffect(() => {
+    fetchWatchlist();
+  },[]); 
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (error) return <div>Error fetching ticker details.</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching ticker details.</div>;
 
   return (
     <div>
-      {watchList.map((ticker, index) => (
+      {watchList?.map((ticker, index) => (
         ticker ? 
         <div key={index}>
           <TickerCard ticker={ticker} />
